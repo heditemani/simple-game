8b00de72f1e55adafa4d66ddb5687dbcfcf0271c
heditemani <hedi.temani97@gmail.com>
2026-04-08 11:08:47 +0100

    styling

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [userChoice, setUserChoice] = useState("Pierre");
+  const [userChoice, setUserChoice] = useState("Select votre choix");
   const [computerChoice, setComputerChoice] = useState("");
   const [wins, setWins] = useState(0);
   const [losses, setLosses] = useState(0);
@@ -14,7 +14,11 @@ export default function Home() {
     const randomChoice = choices[Math.floor(Math.random() * choices.length)];
     setComputerChoice(randomChoice);
 
-    if (userChoice === randomChoice) {
+    if (userChoice === "Selectionner votre choix"){
+
+      setResult("SVP Choisir votre choix")
+
+    } else if (userChoice === randomChoice) {
       setResult("Égalité !");
     } else if (
       (userChoice === "Pierre" && randomChoice === "Ciseaux") ||
@@ -31,15 +35,16 @@ export default function Home() {
 
   return (
     <div className="main-container" style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Let's Play</h1>
+      <h1>Pierre Papier Ciseaux</h1>
 
       <select value={userChoice} onChange={(e) => setUserChoice(e.target.value)}>
+        <option value='Selectionner votre choix'>Selectionner votre choix</option>
         <option value='Pierre'>Pierre</option>
         <option value='Feuille'>Feuille</option>
         <option value='Ciseaux'>Ciseaux</option>
       </select>
 
-      <button onClick={Submit}>Go..</button>
+      <button className="btn" onClick={Submit}>Jouée</button>
 
       <div>
         <p>Ordinateur a choisi : <strong>{computerChoice}</strong></p>
